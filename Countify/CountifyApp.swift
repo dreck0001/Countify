1b8c377f5ad833681e017526a5b9b7c5536bfe0f
Denis Ansah <Denis.Ansah@yahoo.com>
2025-02-16 23:25:15 -0500

    Initial Commit

---
+++ Countify/CountifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountifyApp.swift
+//  Countify
+//
+//  Created by Throw Catchers on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CountifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountifyTests/CountifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountifyTests.swift
+//  CountifyTests
+//
+//  Created by Throw Catchers on 2/16/25.
+//
+
+import Testing
+@testable import Countify
+
+struct CountifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
