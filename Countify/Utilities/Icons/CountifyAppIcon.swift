21950af0605e26d80cfaa87b76602c859137feec
Denis Ansah <Denis.Ansah@yahoo.com>
2025-02-28 19:40:52 -0500

    Increased +/- sizes and added an app icon view and image.

---
+++ Countify/Utilities/Icons/CountifyAppIcon.swift
@@ -0,0 +1,18 @@
+//
+//  CountifyAppIcon.swift
+//  Countify
+//
+//  Created by Throw Catchers on 2/27/25.
+//
+
+import SwiftUI
+
+struct CountifyAppIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountifyAppIcon()
+}
